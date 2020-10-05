00bc0225c1f906fc797c064f07c271638d0433dc
Vish Singh <vishalicious213@gmail.com>
2020-10-05 21:21:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Modify the assignment statements below so that an integer is assigned to "my_int", a floating point is assigned to "my_float", and a string is assigned to "my_string".
+
+Then, use typecasting to print your integer as a floating point number. Use typecasting again to print your floating point number as an integer.
+"""
+
+# Modify the code below to fulfill the requirements outlined in the docstring.
+my_int = int(213.01)
+my_float = float(213)
+my_string = "hello"
+
+# Practice typecasting as outlined above with these print statements.
+print("integer", my_int)
+print("float", my_float)
+
+# just learned that we can add text and a comma to combine text and variables, like JS
